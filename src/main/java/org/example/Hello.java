66784c364008ae928e21b47cff1e37b4c9e78e84
Driss Biaz <drissbiaz1@gmail.com>
2023-01-19 21:03:50 +0100

    delete

--- src/main/java/org/example/Hello.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Hello {
-
-    void  sayHello(){
-        System.out.println("Hello Driss");
-        System.out.println("Hello Zineb");
-    }
-}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Hello hello= new Hello();
-        hello.sayHello();
+
     }
 }
